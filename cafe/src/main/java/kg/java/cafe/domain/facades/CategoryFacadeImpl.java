e5e0579cef2ef3e7180e640044effbacc5eee1d3
User <stparchkg@gmail.com>
2023-12-08 22:34:07 +0600

    add CategoryFacade

---
+++ cafe/src/main/java/kg/java/cafe/domain/facades/CategoryFacadeImpl.java
@@ -0,0 +1,38 @@
+package kg.java.cafe.domain.facades;
+
+import kg.java.cafe.core.contracts.facades.CategoryFacade;
+import kg.java.cafe.core.contracts.services.CategoryService;
+import kg.java.cafe.core.exceptions.EntityDuplicateException;
+import kg.java.cafe.core.exceptions.EntityNotFoundException;
+import kg.java.cafe.core.models.dtos.category.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryFacadeImpl implements CategoryFacade {
+    private final CategoryService categoryService;
+
+    public CategoryFacadeImpl(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public CategoryDto add(CreateCategoryDto model) throws EntityDuplicateException {
+        return categoryService.add(model);
+    }
+
+    @Override
+    public CategoryDto update(UpdateCategoryDto model) throws EntityNotFoundException {
+        return categoryService.update(model);
+    }
+
+    @Override
+    public HttpStatus delete(DeleteCategoryDto model) throws EntityNotFoundException {
+        return categoryService.delete(model);
+    }
+
+    @Override
+    public CategoryDto findById(FindByIdCategoryDto model) throws EntityNotFoundException {
+        return categoryService.findById(model);
+    }
+}
