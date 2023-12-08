f2dd80265d2ab874b0e42276dad760c5eb40b8a9
User <stparchkg@gmail.com>
2023-12-08 23:15:05 +0600

    add DishService

---
+++ cafe/src/main/java/kg/java/cafe/core/contracts/services/DishService.java
@@ -0,0 +1,13 @@
+package kg.java.cafe.core.contracts.services;
+
+import kg.java.cafe.core.exceptions.EntityDuplicateException;
+import kg.java.cafe.core.exceptions.EntityNotFoundException;
+import kg.java.cafe.core.models.dtos.dish.*;
+import org.springframework.http.HttpStatus;
+
+public interface DishService {
+    DishDto add(CreateDishDto model) throws EntityNotFoundException, EntityDuplicateException;
+    DishDto update(UpdateDishDto model) throws EntityNotFoundException;
+    HttpStatus delete(DeleteDishDto model) throws EntityNotFoundException;
+    DishDto findById(FindByIdDishDto model) throws EntityNotFoundException;
+}
---
+++ cafe/src/main/java/kg/java/cafe/domain/services/DishServiceImpl.java
@@ -0,0 +1,62 @@
+package kg.java.cafe.domain.services;
+
+import kg.java.cafe.core.contracts.services.DishService;
+import kg.java.cafe.core.exceptions.EntityDuplicateException;
+import kg.java.cafe.core.exceptions.EntityNotFoundException;
+import kg.java.cafe.core.mappers.DishMapper;
+import kg.java.cafe.core.models.dtos.dish.*;
+import kg.java.cafe.data.CategoryRepository;
+import kg.java.cafe.data.DishRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishServiceImpl implements DishService {
+    private final DishRepository dishRepository;
+    private final DishMapper dishMapper;
+    private final CategoryRepository categoryRepository;
+
+    public DishServiceImpl(DishRepository dishRepository, DishMapper dishMapper, CategoryRepository categoryRepository) {
+        this.dishRepository = dishRepository;
+        this.dishMapper = dishMapper;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public DishDto add(CreateDishDto model) throws EntityNotFoundException, EntityDuplicateException {
+        var category = categoryRepository.findById(model.getCategoryId()).orElseThrow(EntityNotFoundException::new);
+        var dish = dishRepository.findDishEntityByName(model.getName());
+        if (dish.isPresent()) throw new EntityDuplicateException();
+        var entity = dishMapper.fromDomain(model);
+        entity.setCategory(category);
+        dishRepository.save(entity);
+        return dishMapper.toDomain(entity);
+    }
+
+    @Override
+    public DishDto update(UpdateDishDto model) throws EntityNotFoundException {
+        var category = categoryRepository.findById(model.getCategoryId()).orElseThrow(EntityNotFoundException::new);
+        var dish = dishRepository.findById(model.getId()).orElseThrow(EntityNotFoundException::new);
+        var entity = dish.toBuilder()
+                .id(model.getId())
+                .name(model.getName())
+                .price(model.getPrice())
+                .category(category)
+                .build();
+        dishRepository.save(entity);
+        return dishMapper.toDomain(entity);
+    }
+
+    @Override
+    public HttpStatus delete(DeleteDishDto model) throws EntityNotFoundException {
+        var dish = dishRepository.findById(model.getId()).orElseThrow(EntityNotFoundException::new);
+        dishRepository.delete(dish);
+        return HttpStatus.OK;
+    }
+
+    @Override
+    public DishDto findById(FindByIdDishDto model) throws EntityNotFoundException {
+        var dish = dishRepository.findById(model.getId()).orElseThrow(EntityNotFoundException::new);
+        return dishMapper.toDomain(dish);
+    }
+}
